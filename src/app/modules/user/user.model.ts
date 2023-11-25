9b3ef6fefdd267170d008545bf6cffbceefcb784
Saifur Rahman Nirob <snirob786@gmail.com>
2023-11-25 23:13:05 +0600

    feat: User schema and model defination

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -4,19 +4,18 @@ import bcrypt from "bcrypt";
 
 const userSchema = new Schema<User>({
   userId: { type: Number },
-  username: { type: String, required: true, unique: true },
+  username: { type: String, unique: true },
   password: {
     type: String,
-    required: true,
     set: (password: string) =>
       bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
   },
   fullName: {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    firstName: { type: String },
+    lastName: { type: String },
   },
   age: { type: Number },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, unique: true },
   isActive: { type: Boolean },
   hobbies: [String],
   address: {
