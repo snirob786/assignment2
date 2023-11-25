c7bc8ed8f07825283301a648ca36d9ab6f68a87b
Saifur Rahman Nirob <snirob786@gmail.com>
2023-11-25 23:10:16 +0600

    feat: Post method for api/users route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -9,4 +9,7 @@ router.get("/:userId", userController.getUser);
 router.get("/:userId/orders", userController.getOrdersByUserId);
 router.get("/:userId/orders/total-price", userController.getTotalByUserId);
 
+// Post methods for the api/users route
+router.post("/", userController.createUser);
+
 export const userRouter = router;
